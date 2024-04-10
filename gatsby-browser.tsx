226084837e7c347c2cb5b28e8a7bd7148c659658
qiaoli116 <qiaoli.116@gmail.com>
2024-04-10 14:34:09 +1000

    init project

---
+++ gatsby-browser.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme'; // Import your created theme
+
+// Wrap your Gatsby app with ThemeProvider
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
+};
---
+++ theme.ts
@@ -0,0 +1,17 @@
+import { createTheme, PaletteOptions, ThemeOptions } from '@mui/material/styles';
+
+const palette: PaletteOptions = {
+  primary: {
+    main: '#007bff', // Your primary color
+  },
+  secondary: {
+    main: '#ffc107', // Your secondary color
+  },
+};
+
+const theme: ThemeOptions = {
+  palette, // Apply your palette configuration
+  // Other theme customizations can go here
+};
+
+export default createTheme(theme);
